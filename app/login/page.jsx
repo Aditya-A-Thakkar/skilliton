0c6f1a8bc2b7a3fb70b54ce539bfe5e1a99ee2bc
Aditya Thakkar <adityaat@iisc.ac.in>
2025-07-12 08:30:44 +0530

    Initial commit

---
+++ app/login/page.jsx
@@ -0,0 +1,8 @@
+"use client";
+import SignIn from '../sign-in/SignIn';
+
+export default function LoginPage() {
+    return (
+        <SignIn></SignIn>
+    );
+};
---
+++ app/register/page.jsx
@@ -0,0 +1,8 @@
+"use client";
+import SignUp from '../sign-up/SignUp';
+
+export default function LoginPage() {
+    return (
+        <SignUp></SignUp>
+    );
+};
